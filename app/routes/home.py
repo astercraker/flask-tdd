be58798509e26ad911a0007ac1b0b59001a3577d
astercraker <venus_bronce@hotmail.com>
2021-09-06 19:02:19 -0500

    navbar design modified

--- app/routes/home.py
+++ app/routes/home.py
@@ -18,3 +18,15 @@ def home():
 @main.route("/<name>")
 def home_name(name):
     return f"Hola {name}"
+
+@main.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
